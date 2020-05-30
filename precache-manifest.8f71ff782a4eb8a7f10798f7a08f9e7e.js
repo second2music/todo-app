ee26292a3adbaa27f09308093203460601e9a346
second <second2music@gmail.com>
2020-05-31 03:47:32 +0900

    Updates

---
+++ precache-manifest.8f71ff782a4eb8a7f10798f7a08f9e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c1aa7237d367ae37f8d7567f2e1730",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "526a9711634f970f2426",
+    "url": "/todo-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7df3621ac4944d2fa80a",
+    "url": "/todo-app/static/js/2.211da471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.211da471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526a9711634f970f2426",
+    "url": "/todo-app/static/js/main.f241fd70.chunk.js"
+  },
+  {
+    "revision": "672d1f4664f6508537da",
+    "url": "/todo-app/static/js/runtime-main.6751aa9a.js"
+  }
+]);
